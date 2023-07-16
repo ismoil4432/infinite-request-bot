63445a77319fc2b015ba059c2e45af033d5e43cd
Ismoil4432 <ismoil4432@gmail.com>
2023-07-16 22:36:14 +0500

    updated

--- src/bot.ts
+++ src/bot.ts
@@ -70,16 +70,15 @@ const sendRequestEveryTime = async (ctx: Context) => {
 // Start the server
 if (process.env.NODE_ENV === 'production') {
 	// Use Webhooks for the production server
-	// const app = express();
-	// app.use(express.json());
-	// app.use(webhookCallback(bot, 'express'));
+	const app = express();
+	app.use(express.json());
+	app.use(webhookCallback(bot, 'express'));
 
-	// const PORT = process.env.PORT || 3000;
-	// app.listen(PORT, () => {
-	// 	console.log(`Bot listening on port ${PORT}`);
-	// });
+	const PORT = process.env.PORT || 3000;
+	app.listen(PORT, () => {
+		console.log(`Bot listening on port ${PORT}`);
+	});
 	bot.start();
-
 } else {
 	// Use Long Polling for development
 	bot.start();
